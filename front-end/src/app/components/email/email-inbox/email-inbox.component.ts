6cbdead23db3541eb1d5959c66c6f0c1e8c421f0
Marcus-Nastasi <vinnie.nsts@gmail.com>
2025-04-28 23:42:10 -0300

    feat: created email-inbox component's base.

---
+++ front-end/src/app/components/email/email-inbox/email-inbox.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'email-inbox',
+  imports: [],
+  templateUrl: './email-inbox.component.html',
+  styleUrl: './email-inbox.component.scss'
+})
+export class EmailInboxComponent {
+
+}
--- front-end/src/app/pages/email/email.component.ts
+++ front-end/src/app/pages/email/email.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { EmailMenuComponent } from '../../shared/components/email-menu/email-menu.component';
+import { EmailInboxComponent } from '../../components/email/email-inbox/email-inbox.component';
 
 @Component({
   selector: 'app-email',
-  imports: [EmailMenuComponent],
+  imports: [EmailMenuComponent, EmailInboxComponent],
   templateUrl: './email.component.html',
   styleUrl: './email.component.scss'
 })
